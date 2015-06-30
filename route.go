75b054a475e6bc949bf5e1b6635351bd3413cf18
vvijay <vvijay@bancoaurora.com>
2015-06-30 13:00:55 -0700

    Some changes

--- route.go
+++ route.go
@@ -100,9 +100,9 @@ func (r *Route) GetHandler() http.Handler {
 
 // Use wraps middlware around the existing handler for a route
 func (r *Route) Use(m func(http.ResponseWriter, *http.Request, http.HandlerFunc)) *Route {
-	f := r.handler.ServeHTTP
+	h := r.handler
 	return r.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		m(w, req, f)
+		m(w, req, h.ServeHTTP)
 	})
 }
 
